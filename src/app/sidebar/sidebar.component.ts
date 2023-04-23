529fe2edf3a29a0ecffeac4f448b283bd0323cd6
Quigonar <67380073+Quigonar@users.noreply.github.com>
2023-04-22 22:22:24 -0600

    updated colors of app

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -24,12 +24,13 @@ export var Client: RouteInfo[] = []
 
 @Component({
   selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html'
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
   menuItems: any[];
 
-  constructor(private user: RouteService) { 
+  constructor(public user: RouteService) { 
     
   }
 
