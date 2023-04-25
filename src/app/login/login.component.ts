2035eea2140a623d834c5d74c26d7f47ab465da7
Quigonar <67380073+Quigonar@users.noreply.github.com>
2023-04-24 22:08:52 -0600

    fixed all sprint 1 errors and suggestions

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -90,72 +90,6 @@ export class LoginComponent implements OnInit {
     this.player = form
     console.log(this.player)
 
-<<<<<<< Updated upstream
-    var type, message, icon
-    const alphanumericRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])/;
-
-    if (form.name === '' || form.username === '' || form.nationality === '' || form.password === '' || form.password_confirmation === '' || form.email === '') {
-      icon = "fa fa-exclamation-triangle"
-      type = "danger"
-      message = "Porfavor asegúrese de que todas las casillas esten llenas, vuelva a intentarlo."
-      this.alert.createAlert(icon,type,message)
-    }
-    else if (form.username.length > 30 || form.username.length < 1) {
-      icon = "fa fa-exclamation-triangle"
-      type = "danger"
-      message = "Porfavor asegúrese de que el usuario contenga entre 1-30 caracteres, vuelva a intentarlo."
-      this.alert.createAlert(icon,type,message)
-    }
-    else if (form.password.length != 8) {
-      icon = "fa fa-exclamation-triangle"
-      type = "danger"
-      message = "Porfavor asegúrese de que la contraseña sea exactamente de 8 caracteres, vuelva a intentarlo."
-      this.alert.createAlert(icon,type,message)
-    }
-    else if (!(alphanumericRegex.test(form.password))) {
-      icon = "fa fa-exclamation-triangle"
-      type = "danger"
-      message = "Porfavor asegúrese de que la contraseña contenga números y letras, vuelva a intentarlo."
-      this.alert.createAlert(icon,type,message)
-    }
-    else if (form.password != form.password_confirmation) {
-      icon = "fa fa-exclamation-triangle"
-      type = "danger"
-      message = "Porfavor asegúrese de que las contraseñas coincidan, vuelva a intentarlo."
-      this.alert.createAlert(icon,type,message)
-    }
-
-    // Tambien hay que revisar si el correo se encuentra o no en la base de datos, devolver error exclusivo para caso de que el correo ya se encuentre en el DB
-    // Si devuelve 1 es que el username existe, 2 correo existe, caulquier otra cosa success
-
-    else {
-      this.player.Nombre = form.name
-      this.player.Contrasena = form.password
-      this.player.Correo = form.email
-      this.player.Username = form.username
-      this.player.Nacionalidad = form.nationality
-
-      //Hacer el post por el API y desde el API responder con el ID dado al jugador y reemplazar this.player.ID
-      this.api.addUser(this.player).subscribe(answer => {
-        if (answer === "2") {
-          icon = "fa fa-exclamation-triangle"
-          type = "danger"
-          message = "El usuario ingresado ya existe, cambielo y vuelva a intentarlo."
-          this.alert.createAlert(icon,type,message)
-        }
-        else if (answer === "1") {
-          icon = "fa fa-exclamation-triangle"
-          type = "danger"
-          message = "El correo ingresado ya existe, cambielo y vuelva a intentarlo."
-          this.alert.createAlert(icon,type,message)
-        } else {
-          icon = "fa fa-check"
-          type = "success"
-          message = "La cuenta ha sido creada exitosamente"
-          this.alert.createAlert(icon,type,message)
-          this.player.ID = answer
-          this.routeService.switch("first", this.player.ID)
-=======
     if (this.infoVerifier.verifyUserInfo(form)) {
       
       //Hacer el post por el API
@@ -163,7 +97,6 @@ export class LoginComponent implements OnInit {
         if (this.infoVerifier.verifyUserAnswer(answer)) {
           this.player.Id = answer
           this.routeService.switch("first", this.player.Id)
->>>>>>> Stashed changes
         }
       })
     }
